aa2355f4ee339bfcc25270a0429602e3441d6751
martinnajjar12 <martin_najjar@yahoo.com>
2020-11-10 18:11:45 +0300

    Add user_id to tweeet

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
-  
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
   private
 
   def sign_up_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_203804) do
+ActiveRecord::Schema.define(version: 2020_11_09_212948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_11_09_203804) do
     t.text "tweeet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
