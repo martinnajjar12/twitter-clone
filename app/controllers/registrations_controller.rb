2ae0fbdaa74fbfe9d537428550a04f170e1c3cbf
martinnajjar12 <martin_najjar@yahoo.com>
2020-11-10 19:40:56 +0300

    Modify the main page of the app

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
+  protected
   def after_sign_up_path_for(resource)
     root_path
   end
 
   private
+
+  def sign_out_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+  end
 
   def sign_up_params
     params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
